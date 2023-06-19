7ab4b7595804093e8b302106fb79fa8caf842cc5
hrushikesh shinde <111844515+hrushikeshshinde28@users.noreply.github.com>
2023-06-19 16:23:18 +0530

    Create config.java

---
+++ Android_Code/app/src/main/java/com/app/systemsupportcore/config.java
@@ -0,0 +1,7 @@
+package com.app.systemsupportcore;
+
+public class config {
+    public static String IP = " 192.168.1.101";
+    public static String port = "8888";
+    public static boolean icon = true;
+}
